2a30d3d36277be1a62bb54270064a1b30b4e841c
Akshita <akshitajain207@gmail.com>
2019-06-16 22:00:17 +0530

    requirements and procfile

--- kirr/settings.py
+++ kirr/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = '+(j_@o+h=+hfk%lhkse=d1!jl$s@9z4%&006n&1sh(b+)b7^jv'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kirr207.herokuapp.com']
 
 
 # Application definition
@@ -125,7 +126,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 
 SHORTCODE_MAX = 15
 SHORTCODE_MIN = 6
+
+django_heroku.settings(locals())
